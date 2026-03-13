220a12d5d5a6c44044afb67cd3e420a8ce10fb86
LuisaAR01 <107075129+LuisaAR01@users.noreply.github.com>
2026-03-13 13:19:47 -0400

    Add user authentication and dashboard routes

--- Sistema de recursos humanos/app.py
+++ Sistema de recursos humanos/app.py
@@ -1,13 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
 import models
+from flask import session
+from functools import wraps
 
 app = Flask(__name__)
+app.secret_key = "gesthum_secreto"
 
 #para conectarlo con el html
 @app.route("/")
 def inicio():
-    return render_template("index.html")
+    if "user_id" in session:
+        return redirect(url_for("dashboard"))
+    else:
+        return redirect(url_for("login"))
+
+# 🔐 Decorador de protección
+def login_requerido(roles=None):
+    def decorador(f):
+        @wraps(f)
+        def funcion_protegida(*args, **kwargs):
+            if 'user_id' not in session:
+                return redirect(url_for('login'))
+
+            if roles and session['rol'] not in roles:
+                return "Acceso no autorizado", 403
+
+            return f(*args, **kwargs)
+        return funcion_protegida
+    return decorador
+
 
 #ruta para los empleados (para mostrar y obtener, etc)
 @app.route('/empleados', methods=['GET', 'POST'])
@@ -76,11 +97,54 @@ def nomina():
     return render_template('nomina.html', empleados=empleados, nominas=nominas)
 
 
+#Pa el login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        usuario = models.validar_usuario(username, password)
+
+        if usuario:
+            session["user_id"] = usuario[0]
+            session["rol"] = usuario[3]
+
+            return redirect(url_for("dashboard"))
+
+        else:
+            return "Usuario o contraseña incorrectos"
+
+    return render_template("login.html")
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+#pal dashboard
+@app.route("/dashboard")
+@login_requerido()
+def dashboard():
+    rol = session['rol']
+
+    if rol == "ADMIN":
+        return render_template("dashboard_admin.html")
+
+    elif rol == "EMPLEADO":
+        return render_template("dashboard_empleado.html")
+
+    else:
+        return "Rol no reconocido"
+
+
 #Pa correr la vaina (SIEMPRE va al final de todas las rutas)
 if __name__ == "__main__":
+    print("INICIANDO SISTEMA GESTHUM")
     models.crear_tabla_empleados()
     models.crear_tabla_usuarios()
     models.crear_tabla_evaluaciones()
     models.crear_tabla_capacitaciones()
     models.crear_tabla_nomina()
+    models.crear_admin_por_defecto()
     app.run(debug=True)
